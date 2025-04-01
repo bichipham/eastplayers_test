b52ce73927568b4435af6db4a9afd5f5cb4e975b
bichp <88999526+bichp@users.noreply.github.com>
2025-04-01 23:44:13 +0700

    - add setp 2

--- src/page/AppointmentPage/SelectPackageModal/index.js
+++ src/page/AppointmentPage/SelectPackageModal/index.js
@@ -1,18 +1,85 @@
-import { Button, Input, Modal, Form, Checkbox, message, Select } from "antd";
+import { Button, Input, Modal, Checkbox, Table, Collapse } from "antd";
 import "./style.css";
 import Image from "next/image";
 import { SearchOutlined } from "@ant-design/icons";
 import { useAppointment } from "../hook";
 import map from "lodash/map";
+import filter from "lodash/filter";
+import find from "lodash/find";
 import isEmpty from "lodash/isEmpty";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { MainContext } from "@/service/StoreContext";
 import iconPackage from "@/assets/images/icoPackage.png";
 import iconOption from "@/assets/images/iconOption.png";
+import iconUp from "@/assets/images/icoup.png";
+import iconDown from "@/assets/images/icodown.png";
 
 const { Search } = Input;
-const SelectPackageModal = ({ showing, onClose }) => {
+const SelectPackageModal = ({ showing, onClose, onSubmit }) => {
   const { listPackage } = useContext(MainContext);
+  const [listSelectItem, setListSelectItem] = useState([]);
+
+  const columns = [
+    {
+      title: "Package Name",
+      dataIndex: "name",
+      key: "name",
+      width: "30%",
+      render: (text, record) => (
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <Image
+            src={record?.isOption ? iconOption : iconPackage}
+            alt="package"
+            width={50}
+            height={50}
+            style={{ marginRight: "5px" }}
+          />
+          {record?.name}
+        </div>
+      ),
+    },
+    {
+      title: "Service",
+      dataIndex: "service",
+      key: "service",
+      width: "30%",
+    },
+    {
+      title: "Price",
+      dataIndex: "price",
+      width: "20%",
+      key: "price",
+    },
+    {
+      title: "Estimate time",
+      dataIndex: "time",
+      width: "10%",
+      key: "time",
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (text, record) => (
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <Checkbox onChange={(e) => onSelectItem(e.target.checked, record)} />
+        </div>
+      ),
+    },
+  ];
+
+  const onSelectItem = (checked, record = []) => {
+    console.log("!!!!!! checked ", checked, record);
+    let list = listSelectItem;
+    if (checked) {
+      list = [...listSelectItem, ...record];
+    } else {
+      const listId = map(record, (r) => r?.id);
+      list = filter(listSelectItem, (item) => !listId?.includes(item?.id));
+    }
+    setListSelectItem(list);
+  };
+
+  const data = listPackage;
 
   return (
     <Modal
@@ -20,24 +87,27 @@ const SelectPackageModal = ({ showing, onClose }) => {
       className="popup-main"
       open={showing}
       onCancel={onClose}
-      width={800}
+      width={1000}
       closable={true}
       footer={null}
       styles={{
-        maxHeight: "1000px",
         minHeigth: "300px",
       }}
     >
       <h2 className="title-h1">Package</h2>
-      <div>
-        <Input
-          placeholder="Search"
-          className="search-left"
-          suffix={<SearchOutlined style={{ color: "rgba(0,0,0,.45)" }} />}
-        />
-      </div>
+
+      <Input
+        style={{ width: "100%", marginBottom: "15px" }}
+        placeholder="Search"
+        suffix={<SearchOutlined style={{ color: "rgba(0,0,0,.45)" }} />}
+      />
+
       <div className="table-style">
-        <table>
+        {/* <Table
+          columns={columns}
+          dataSource={data}
+        /> */}
+        {/* <table>
           <thead>
             <tr>
               <th className="text-left w350">Package Name</th>
@@ -51,7 +121,7 @@ const SelectPackageModal = ({ showing, onClose }) => {
             {map(listPackage, (item) => (
               <tr key={item?.id}>
                 <td className="text-left w350">
-                  <div style={{display: 'flex', alignItems: 'center'}} >
+                  <div style={{ display: "flex", alignItems: "center" }}>
                     <Image
                       src={iconPackage}
                       alt="package"
@@ -66,22 +136,35 @@ const SelectPackageModal = ({ showing, onClose }) => {
                 <td className="text-left w250">{item?.price}</td>
                 <td className="text-left w250">{item?.time}</td>
                 <td className="text-left w150">
-                  {/* <Checkbox
-                    checked={item?.id === selectItem?.id}
-                    onChange={(e) =>
-                      onChangeSelectRequest(e.target.checked, item)
-                    }
-                  /> */}
+                  <Collapse
+                    items={[
+                      {
+                        key: "1",
+                        label: "expand",
+                        children: <p>{'text'}</p>,
+                      },
+                    ]}
+                  />
                 </td>
               </tr>
             ))}
           </tbody>
-        </table>
+        </table> */}
+        <div>
+          {map(listPackage, (item) => (
+            <RowBlock
+              item={item}
+              key={item?.id}
+              listSelectItem={listSelectItem}
+              onSelectItem={onSelectItem}
+            />
+          ))}
+        </div>
         <div className="action-btn">
           <Button type="primary" color="blue" ghost onClick={onClose}>
             Cancel
           </Button>
-          <Button type="primary">Next</Button>
+          <Button type="primary" onClick={() => onSubmit(listSelectItem)}>Next</Button>
         </div>
       </div>
     </Modal>
@@ -89,3 +172,102 @@ const SelectPackageModal = ({ showing, onClose }) => {
 };
 
 export default SelectPackageModal;
+
+const RowBlock = ({ item, onSelectItem, listSelectItem }) => {
+  console.log("!!!!!1 listSelectItem ", listSelectItem);
+  const [expand, setExpand] = useState(true);
+  const [selectAll, setSelectAll] = useState(false);
+  const haveChildren = !isEmpty(item?.children);
+  const isSelected = (id) => find(listSelectItem, (record) => record?.id == id);
+  useEffect(() => {
+    if (selectAll) {
+      onSelectItem(true, item?.children);
+    } else {
+      onSelectItem(false, item?.children);
+    }
+  }, [selectAll]);
+  return (
+    <>
+      <div
+        className={`list-row-item ${
+          haveChildren && expand ? "expandable" : ""
+        } ${isSelected(item?.id) ? "selected" : ""}`}
+      >
+        <RowItem item={item} />
+        <div style={{ width: "150px", textAlign: "end" }}>
+          {haveChildren ? (
+            <Image
+              src={!expand ? iconDown : iconUp}
+              width={30}
+              height={30}
+              alt=""
+              onClick={() => setExpand(!expand)}
+							style={{float: 'right'}}
+            />
+          ) : (
+            <Checkbox
+              onChange={(e) => onSelectItem(e.target.checked, [item])}
+            />
+          )}
+        </div>
+      </div>
+      {haveChildren && expand ? (
+        <>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "flex-end",
+            }}
+          >
+            <a onClick={() => setSelectAll(!selectAll)}>{`${
+              selectAll ? "Unselect All" : "Select All"
+            }`}</a>
+          </div>
+          {map(item?.children, (children) => (
+            <div
+              className={`list-row-item
+						 ${isSelected(children?.id) ? "selected" : ""}`}
+            >
+              <RowItem item={children} />
+              <div style={{ width: "100px", textAlign: "end" }}>
+                <Checkbox
+                  onChange={(e) => onSelectItem(e.target.checked, [children])}
+                  checked={isSelected(children?.id)}
+                />
+              </div>
+            </div>
+          ))}
+        </>
+      ) : null}
+    </>
+  );
+};
+
+const RowItem = ({ item }) => {
+  return (
+    <>
+      <div
+        style={{ display: "flex", alignItems: "center", width: "350px" }}
+        className="text-left"
+      >
+        <Image
+          src={item?.isOption ? iconOption : iconPackage}
+          alt="package"
+          width={50}
+          height={50}
+          style={{ marginRight: "10px" }}
+        />
+        {item?.name}
+      </div>
+      <div className="text-left" style={{ width: "350px" }}>
+        {item?.service}
+      </div>
+      <div className="text-left" style={{ width: "200px" }}>
+        {item?.price}
+      </div>
+      <div className="text-left" style={{ width: "200px" }}>
+        {item?.time}
+      </div>
+    </>
+  );
+};
